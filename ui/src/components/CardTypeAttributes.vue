5f5b60e249772d9739da488e9734f402e18dfb60
Vinnie Pauley <vinniepauley@gmail.com>
2021-09-05 19:28:45 -0500

    build auto-constructed card type options

--- ui/src/components/CardTypeAttributes.vue
+++ ui/src/components/CardTypeAttributes.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     Card Type Attributes
-    <p style="border:1px solid black;">{{attributeMap}}</p>
-    <p style="border:1px solid blue;">{{ attributeOptions }}</p>
+    <ul>
+      <li v-for="level in configuredData" :key="level.datapoint">
+        <h5>{{ level.datapoint }}</h5>
+        <p>{{ level.options }}</p>
+      </li>
+    </ul>
+    <!-- <SelectFromList
+          label="Brand"
+          name="brand"
+          :options="brandOptionsList"
+          @selection="brandSelected"
+    />-->
   </div>
 </template>
 
@@ -16,6 +26,28 @@ export default {
     attributeOptions: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    getOptionsForDataPoint(options) {
+      const match = this.attributeOptions.filter(x => {
+        if (x[options]) return x[options];
+      })[0];
+
+      if (match) {
+        return match[options];
+      }
+    }
+  },
+  computed: {
+    configuredData() {
+      return this.attributeMap.map(a => {
+        const datapoint = a.attribute;
+        return {
+          datapoint,
+          options: this.getOptionsForDataPoint(a.options)
+        };
+      });
     }
   }
 };
